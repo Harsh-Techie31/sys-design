e731a6efe3d9b6993e8b3d82cb93623b7090bdd6
Harsh <harshchoudhary3113@gmail.com>
2026-04-19 14:53:32 +0530

    this version follows the SOLID principles

--- google docs editor/second.cpp
+++ google docs editor/second.cpp
@@ -37,7 +37,7 @@ class ImageElement  : public DocumentElement{
     }
 
     string render() override{
-        string temp = "Image is : [" + this->path + "]\n";
+        string temp = "\nImage is : [" + this->path + "]\n";
         return temp;
     }
 };
@@ -56,9 +56,115 @@ class NewlineElement : public DocumentElement{
     }
 };
 
+
+class Document{
+
+    private : 
+    vector<DocumentElement*> docs;
+    public : 
+
+    
+    string rendered;
+
+
+    string render(){
+        if(!rendered.empty()){
+            return rendered;
+        }else{
+            for(const auto ele : docs){
+                rendered+= ele->render();
+            }
+        }
+        return rendered;
+    }
+
+    void addElement(DocumentElement* ele){
+            docs.push_back(ele);
+    }
+
+
+};
+
+
+class Persistence{
+
+    public : 
+
+    virtual void save(string text)=0;
+
+};
+
+class save2file : public Persistence{
+    public :
+    void save(string text){
+        ofstream file("result2.txt");
+            if(file.is_open()){
+                file << text ;
+                file.close();
+                cout<<"Elements saved to db\n";
+            }else{cout<<"Error opening the file\n";
+            }
+               
+    }
+};
+
+class save2db : public Persistence{
+    public : 
+    void save(string text){
+        cout<<"Saved the string : "<<text<<" into the Database.";
+    }
+};
+
+
+
+class DocumentEditor{
+
+    private : 
+    Document* doc;
+    string renderedString;
+    Persistence* pers;
+    public : 
+    DocumentEditor(Document* d, Persistence* p){
+        doc = d;
+        pers = p;
+    }
+    void addText(string text){
+        doc->addElement(new TextElement(text));
+    }
+
+    void addImage(string path){
+        doc->addElement(new ImageElement(path));
+    }
+
+    void newLineElement(){
+        doc->addElement(new NewlineElement());
+    }
+
+    string renderDocument(){
+        if(renderedString.empty()) renderedString = doc->render();
+        return renderedString;
+    }
+
+    void saveDocument(){
+        pers->save(renderDocument());
+    }
+
+};
+
 int main()
 {   TextElement te("hello world");
     ImageElement imageElement("youtube.png");
     NewlineElement();
     cout<<"hello world";
+
+    save2file fileSaver;
+    Document newdoc;
+    DocumentEditor de(&newdoc,&fileSaver);
+    de.addText("This is the heading of this file");
+    de.newLineElement();
+    de.addText("THis must be the first line of myy paragraph in the db");
+    de.addImage("in the onderive/images/src/ddcl/23");
+    de.saveDocument();
+
+    
 }
